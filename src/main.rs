b2798fd9f4127d64e468c0f3776fd6973a59370a
DarkSky <darksky2048@gmail.com>
2019-12-16 13:59:14 +0800

    cleanup code

--- src/main.rs
+++ src/main.rs
@@ -2,18 +2,26 @@ mod lib;
 
 use lib::Mp4Muxer;
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, Cursor, Read};
+use std::path::Path;
+
+fn read_file<P: AsRef<Path>>(path: P) -> io::Result<Vec<u8>> {
+    let mut buf = vec![];
+    File::open(path)?.read_to_end(&mut buf)?;
+    Ok(buf)
+}
 
 fn main() {
-    let mut mp4muxer = Mp4Muxer::new(File::create("1.mp4").unwrap());
-    let mut h264 = Vec::new();
-    File::open("1.264").unwrap().read_to_end(&mut h264).unwrap();
-    let mut h265 = Vec::new();
-    File::open("1.265").unwrap().read_to_end(&mut h265).unwrap();
-    mp4muxer.init_video(1920, 1200, false, "test1");
-    mp4muxer.write_video(&h264);
-    mp4muxer.init_video(1920, 1200, true, "test2");
-    mp4muxer.write_video(&h265);
+    let buffer = Cursor::new(vec![]);
+    let mut mp4muxer = Mp4Muxer::new(buffer);
+    if let Ok(h264) = read_file("1.264") {
+        mp4muxer.init_video(1920, 1200, false, "h264 stream");
+        mp4muxer.write_video(&h264);
+    }
+    if let Ok(h265) = read_file("1.265") {
+        mp4muxer.init_video(1920, 1200, false, "h265 stream");
+        mp4muxer.write_video(&h265);
+    }
     mp4muxer.write_comment("test comment");
     mp4muxer.close();
 }
